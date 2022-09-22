b9878a2ef1e17af11324135306b5b09b9f275b03
alonsocanov <alonsocanov@outlook.com>
2022-09-22 14:05:28 -0500

    Add images for segmentations

--- image_processing.py
+++ image_processing.py
@@ -20,7 +20,7 @@ def resize(img: np.ndarray, dim: tuple, factor: float = 1):
     h, w = img.shape[:2]
     if not dim:
         h, w = h * factor, w * factor
-        max_h, max_w = 600, 600
+        max_h, max_w = 1000, 1000
         if h > max_h:
             factor = (max_h / h)
         if w * factor > max_w:
@@ -36,20 +36,20 @@ def setToBkgDim(dim_bkg: tuple, dim_fgd: tuple):
     max_w, max_h = dim_bkg
     w, h = dim_fgd
     factor = 1
+
     if h > max_h:
         factor = ((max_h - 1) / h)
     if w * factor > max_w:
         factor = ((max_w - 1) / w)
-    print(factor)
     return int(w * factor), int(h * factor)
 
 
 def disort(dim: tuple):
     w, h = dim
     pt_x1, pt_y1 = 0, 0
-    pt_x2, pt_y2 = int(w * .5), 0
-    pt_x3, pt_y3 = 0, int(h * .5)
-    pt_x4, pt_y4 = int(w * .5), int(h * .5)
+    pt_x2, pt_y2 = int(w * .99), 0
+    pt_x3, pt_y3 = 0, int(h * .99)
+    pt_x4, pt_y4 = int(w * .99), int(h * .99)
     pts = [[pt_x1, pt_y1], [pt_x2, pt_y2], [pt_x3, pt_y3], [pt_x4, pt_y4]]
     return np.float32(pts)
 
@@ -58,9 +58,14 @@ def createImg(foreground: np.ndarray, background: np.ndarray):
 
     h_back, w_back = background.shape[:2]
     h_fore, w_fore = foreground.shape[:2]
-
     w_fore, h_fore = setToBkgDim((w_back, h_back), (w_fore, h_fore))
     foreground = resize(foreground, (w_fore, h_fore))
+
+    # Clasification segmented image with class (0 - background, 255 - document)
+    # create a black background for segmentation
+    bkg_seg = np.zeros((h_back, w_back, 3), dtype=np.float32)
+    # crete document interest for segmentation
+    fore_seg = np.ones((h_fore, w_fore, 3), dtype=np.float32)
 
     pts_src = np.float32([[0, 0], [w_fore, 0], [0, h_fore], [w_fore, h_fore]])
 
@@ -69,14 +74,17 @@ def createImg(foreground: np.ndarray, background: np.ndarray):
     matrix = cv2.getPerspectiveTransform(pts_src, pts_dst)
 
     new_img = cv2.warpPerspective(
-        foreground, matrix, (int(w_back), int(h_back)))
+        fore_seg, matrix, (int(w_back), int(h_back)))
 
-    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    ret, mask = cv2.threshold(gray, 10, 25, cv2.THRESH_BINARY)
+    # print(np.uint8(background), np.uint8(new_img))
+
+    # new_img = cv2.add(np.uint8(background), np.uint8(new_img))
+
+    # gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    # ret, mask = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
 
     # dst = cv2.addWeighted(background, 1, new_img, .5, 0)
-
     # print(dst)
     # new_h, new_w = new_img.shape[:2]
 
-    return mask
+    return new_img
